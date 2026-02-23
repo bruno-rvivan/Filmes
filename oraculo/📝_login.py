a51a88fb4dd652da7bed67a8b1f52df32e75fdcf
bruno-vivan <157636808+bruno-rvivan@users.noreply.github.com>
2026-02-23 00:11:58 -0300

    Implement get_project_root function
    Add function to get the project root directory

--- oraculo/📝_login.py
+++ oraculo/📝_login.py
@@ -17,6 +17,8 @@ st.set_page_config(
 st.session_state['criando'] = False
 st.session_state['logado'] = False
 
+def get_project_root():
+    return Path(__file__).resolve().parent
 
 st.title('Welcome')
 

a6e9632fc5c0c5edfaee505ef1f641132cd1bae1
bruno-vivan <157636808+bruno-rvivan@users.noreply.github.com>
2026-04-03 15:50:52 -0300

    Refactor login.py to streamline user flow

--- oraculo/📝_login.py
+++ oraculo/📝_login.py
@@ -1,5 +1,4 @@
 import streamlit as st
-import sysimport streamlit as st
 import sys
 from pathlib import Path
 
@@ -26,69 +25,6 @@ def login():
         usuarios = {usuario.name: usuario for usuario in usuarios}
         name = st.text_input(
         'User'
-        )
-        password = st.text_input('password', type="password")
-        if st.button('Login'):
-            user = usuarios[name]
-            if user.desencode(senha=password):
-                st.success('Login completed')
-                st.session_state['usuario'] = user
-                st.session_state['id'] = user.id
-                st.session_state['logado'] = True
-                st.switch_page('pages/1_🏠_home.py')
-            else:
-                st.error('Incorect Password')
-
-# Create account
-
-def create():
-    with st.container(border=True):
-        st.markdown('Create account')
-        name = st.text_input('Username')
-        password = st.text_input('Password')
-        email = st.text_input('Email')
-        if st.button('Create'):
-            create_user(name=name,
-                        password=password,
-                        email=email
-            )
-            st.success('Account created')
-
-
-if __name__ == '__main__':
-    with st.sidebar:
-        option = st.selectbox('Options', ['Login', 'Create'])
-    if option == 'Login':
-        login()
-    else:
-        create()
-from pathlib import Path
-
-ROOT_DIR = Path(__file__).resolve().parents[2]
-sys.path.append(str(ROOT_DIR))
-from db.session import le_todos_users, create_user
-from db.init_db import init_db
-
-init_db()
-
-st.set_page_config(
-    layout='wide',
-    page_icon='📝'
-)
-
-st.session_state['criando'] = False
-st.session_state['logado'] = False
-
-st.title('Welcome')
-
-def login():
-    with st.container(border=True):
-        usuarios = le_todos_users()
-        usuarios = {usuario.name: usuario for usuario in usuarios}
-        name = st.text_input(
-        'User',
-            list(usuarios.keys()
-                 )
         )
         password = st.text_input('password', type="password")
         if st.button('Login'):
